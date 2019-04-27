40188c5ede13daea970d038c8fb59840c57cb028
tiagovieirasoares12@gmail.com <tiagovieirasoares12@gmail.com>
2019-04-27 07:10:04 -0300

    Adicionando projeto

---
+++ Pessoas/Pessoas/Pessoas.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pessoas
+{
+    public class Pessoas
+    {
+        public int IdPessoa { get; set; }
+        public string NomePessoa { get; set; }
+        public string EmailPessoa { get; set; }
+        public string TelefonePessoa { get; set; }
+
+        public List<Pessoas> GerarPessoa()
+        {
+            List<Pessoas> ListaPessoas = new List<Pessoas>();
+
+            try
+            {
+                Pessoas pessoa1 = new Pessoas
+                {
+                    IdPessoa = 1,
+                    NomePessoa = "Pessoa 1",
+                    EmailPessoa = "email1@email1.com",
+                    TelefonePessoa = "111111111"
+                };
+
+                ListaPessoas.Add(pessoa1);
+
+                Pessoas pessoa2 = new Pessoas
+                {
+                    IdPessoa = 2,
+                    NomePessoa = "Pessoa 2",
+                    EmailPessoa = "email1@email2.com",
+                    TelefonePessoa = "222222222"
+                };
+
+                ListaPessoas.Add(pessoa2);
+
+                return ListaPessoas;
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ Pessoas/Pessoas/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pessoas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<Pessoas> ListaPessoas = new List<Pessoas>();
+                Pessoas pessoas = new Pessoas();
+
+                ListaPessoas = pessoas.GerarPessoa();
+
+                foreach(var p in ListaPessoas)
+                {
+                    Console.WriteLine(p.IdPessoa);
+                    Console.WriteLine(p.NomePessoa);
+                    Console.WriteLine(p.EmailPessoa);
+                    Console.WriteLine(p.TelefonePessoa);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
